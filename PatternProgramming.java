7e2de77dd1e71f4a8c0cb23a7042046d8d6a1b45
projectsJS <tanishqraina@gmail.com>
2023-08-08 20:33:54 +0530

    A program is removed

--- PatternProgramming.java
+++ PatternProgramming.java
@@ -17,19 +17,7 @@ public class PatternProgramming {
 //		  System.out.println();
 //		}
 	 
-		/*Program 2 */
-		
-//		for(int i = 0; i<n; i++) {
-//			  for(int j =0; j<n; j++) {
-//				  if(i==0 || j==0 || j==n-1 || i==(n-1)/2) {
-//					  System.out.print("*");
-//				  }else {
-//					  System.out.print(" ");
-//				  }
-//			  }
-//			  System.out.println();
-//			}
-		
+	
 		/*Program 3*/
 //		
 //		for(int i = 0; i<n; i++) {
